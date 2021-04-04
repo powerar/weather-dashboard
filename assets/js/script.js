8d148b077ca22f6798d11f90d6d715a5ae0ab2d9
powerar <arpower@bell.net>
2021-04-04 13:09:16 -0400

    6-day forecast functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,32 +15,60 @@ var getWeather = function (city) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayWeather(data);
-                getUvi(data);
+                getForecast(data);
             });
         }
     });
 };
 
-var getUvi = function (geo) {
+var getForecast = function (geo) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + geo.coord.lat + "&lon=" + geo.coord.lat + "&appid=81e7518916e98ea8c25fc5e8ce330a50";
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                displayUvi(data);
-                console.log(data);
+                displayForecast(data);
             })
         }
     });
 };
 
-var displayUvi = function (uvi) {
+var displayForecast = function (forecast) {
+    debugger;
+    for (i = 1; i < forecast.daily.length; i++) {
+        var forecastEl = document.createElement("div");
+        forecastEl.classList.add("card");
+        $(".card-group").append(forecastEl);
+        var dailyEl = document.createElement("h3");
+        forecastEl.appendChild(dailyEl);
+        var bodyEl = document.createElement("div");
+        bodyEl.classList.add("card-body");
+        forecastEl.appendChild(bodyEl);
+
+        var date = forecast.daily[i].dt * 1000;
+        console.log(date);
+
+        dailyEl.classList.add("card-title");
+        dailyEl.textContent = moment(date).format("MMMM Do YYYY");
+    
+        var tempEl = document.createElement("p");
+        tempEl.textContent = "Temp: " + forecast.daily[i].temp.max + "℃";
+        bodyEl.appendChild(tempEl);
+    
+        var windEl = document.createElement("p");
+        windEl.textContent = "Wind: " + forecast.daily[i].wind_speed + " km/h"
+        bodyEl.appendChild(windEl);
+    
+        var humidEl = document.createElement("p");
+        humidEl.textContent = "Humidity: " + forecast.daily[i].humidity; + "%"
+        bodyEl.appendChild(humidEl);
+    };
+
     var uviEl = document.createElement("p");
-    uviEl.textContent = "UV: " + uvi.current.uvi;
+    uviEl.textContent = "UV: " + forecast.current.uvi;
     currentEl.appendChild(uviEl);
 };
 
 var displayWeather = function (weather) {
-    debugger;
     var cityEl = document.createElement("h4");
     cityEl.classList = "card-title";
     cityEl.textContent = weather.name + " - " + moment().format('MMMM Do YYYY');
